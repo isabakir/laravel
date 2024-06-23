d85d4e7bf75091be47d8cb2c34799baaf05d489d
İsa Bakır <isabakir45@gmail.com>
2024-06-23 10:42:40 +0300

    try

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -259,7 +259,8 @@ class ProductController extends Controller
             $myProduct=[];
             foreach($products as $product){
                $category_name= $this->getCategoriesDetail($product["main_category"]);
-               // dd($product);
+              // $attributes=
+                dd($product);
                 $myProduct[]=[
                     "approved"=>$product["status"]=="A"?true:false,
                     "archived"=>$product["status"]=="H"?true:false,
